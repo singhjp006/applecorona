5bdc1bed800bd7a9cef93cf79cf0862ae7a7ab67
singhjp <jashanpreet.singh@chaipoint.com>
2020-04-19 00:28:21 +0530

    initial commit

---
+++ src/main/java/com/corona/apple/CoronaAppleApplication.java
@@ -0,0 +1,29 @@
+package com.corona.apple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.apple.corona"})
+@EnableJpaRepositories
+public class CoronaAppleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaAppleApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET");
+            }
+        };
+    }
+}
+
