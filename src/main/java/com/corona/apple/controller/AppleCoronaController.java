086f4fafa8a422786c4f242ad7b41d0fe6dc15f9
singhjp <jashanpreet.singh@chaipoint.com>
2020-04-19 22:59:24 +0530

    amazon s3

--- src/main/java/com/corona/apple/controller/AppleCoronaController.java
+++ src/main/java/com/corona/apple/controller/AppleCoronaController.java
@@ -1,14 +1,33 @@
 package com.corona.apple.controller;
 
+import com.corona.apple.service.HelperClass;
+import io.swagger.annotations.ApiOperation;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @RequestMapping("/apple-corona")
 public class AppleCoronaController {
 
+    @Autowired
+    HelperClass helperClass;
+
+    @ApiOperation(response = String.class, value = "uploadFileToS3")
+    @PostMapping(path = "/uploadFile")
+    public @ResponseBody String uploadFileToS3(@RequestParam MultipartFile productImage) throws IOException {
+        return helperClass.uploadFileToS3(productImage);
+    }
+
+    @ApiOperation(response = String.class, value = "uploadFileToS3FromUrl")
+    @PostMapping(path = "uploadFileToS3FromUrl")
+    public @ResponseBody String uploadFileToS3FromUrl(@RequestParam String imageUrl) throws IOException {
+        return helperClass.uploadFileToS3FromUrl(imageUrl);
+    }
 
 }
---
+++ src/main/java/com/corona/apple/service/HelperClass.java
@@ -0,0 +1,54 @@
+package com.corona.apple.service;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
+import java.util.Date;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class HelperClass {
+
+    public String uploadFileToS3(MultipartFile productImage) throws IOException {
+
+        return AmazonS3Helper.uploadFileToS3(convertMultipartToFile(productImage), convertMultipartToFile(productImage).getName(), "apple-corona-product-images");
+    }
+
+    private byte[] convertFileToBytesArray(File file) throws IOException {
+        byte[] bytesArray = new byte[(int) file.length()];
+
+        FileInputStream fis = new FileInputStream(file);
+        fis.read(bytesArray); // read file into bytes[]
+        fis.close();
+        return bytesArray;
+    }
+
+    private File convertMultipartToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File((new Date()).toString());
+
+        //I think we need not to do this as well, will try
+        convertedFile.createNewFile();
+
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
+
+    public String uploadFileToS3FromUrl(String imageUrl) throws IOException {
+        URL url = new URL(imageUrl);
+        Image image = ImageIO.read(url);
+
+        String fileName = url.getFile().substring(url.getFile().lastIndexOf("/") + 1);
+
+        InputStream is = url.openStream();
+
+        return AmazonS3Helper.uploadFileToS3(is, fileName, "apple-corona-product-images");
+    }
+}
